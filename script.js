e51e20ef68e5b75cf4616af49c5699476218ad26
roxetter3916 <114585727+roxetter3916@users.noreply.github.com>
2022-09-28 20:56:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Roxette Romero"; // update the part BETWEEN the "quotes"
+let userName = "roxetter3916"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
